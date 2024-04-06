ea09299e0492f916f8a7ff257e2fb3a9e2fea614
Jaden Johnson <146070033+ATacoDev@users.noreply.github.com>
2024-04-05 23:23:53 -0700

    Add files via upload

--- DiceRoller/shop.h
+++ DiceRoller/shop.h
@@ -1,23 +1,24 @@
 #ifndef SHOP_H
 #define SHOP_H
+
+#include <iostream>
+#include <vector>
 #include "upgrade.h"
 
 using namespace std;
 
 class shop {
+public:
+    // Constructor to initialize upgrades
+    shop();
 
-    public:
-    upgrade upgrades[2] = { // array of upgrades
-        upgrade(10, 0, "Buff Points", "Increase your points from each roll by 1"),
-        upgrade(20, 0, "Buff Health", "Increase your health by 1")
-    };
-    
     void showUpgrades();
     void enterShop();
     void printRollBuff();
-
     int getBuffPoints();
 
+private:
+    vector<upgrade> upgrades;
 };
 
 #endif
